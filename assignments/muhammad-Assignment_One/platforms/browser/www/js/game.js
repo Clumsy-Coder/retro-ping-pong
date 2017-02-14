96885f2fb3799eb483704a38f853dd74135f5a94
Clumsy-Coder <ubari94@gmail.com>
2017-02-14 10:06:22 -0700

    Get the ball moving and bounce of the sides
    - Some fine tuning needs to be done when bouncing
      the ball of the right side.
      * The ball goes of screen before bouncing of the right side.

--- assignments/muhammad-Assignment_One/platforms/browser/www/js/game.js
+++ assignments/muhammad-Assignment_One/platforms/browser/www/js/game.js
@@ -7,12 +7,15 @@ var p1Paddle;
 var p2Paddle;
 const paddleWidth = 100;
 const paddleHeight = 15;
+var paddleY_offset = 0.10;
 
 //ball
 var ballXcoord;
 var ballYcoord;
 const ballWidth = 20;
 const ballHeight = 20;
+var ballXSpeed = 5;
+var ballYSpeed = 5;
 
 //score
 var p1Score;
@@ -24,21 +27,19 @@ var p1Name;
 //frames per second
 const FPS = 60;
 
-//functions
-
+////////////////////////////
 
 window.onload = function()
 {
     $("#gameCanvas").attr("width", window.innerWidth);
     $("#gameCanvas").attr("height", window.innerHeight);
     
-    console.log(document.getElementById("gameCanvas"));
     canvas = document.getElementById("gameCanvas");
     cContext = canvas.getContext("2d");
     //set the ball x and y coordinate
     ballXcoord = canvas.width / 2;
     ballYcoord = canvas.height / 2;
-    setInterval(init(), 1000 / FPS);
+    setInterval(init, 1000/FPS);
 }
 
 function init()
@@ -55,25 +56,51 @@ function setup()
     
     //create the paddle
     //player 1 paddle, bottom
-    createBox(canvas.width/2 - (paddleWidth / 2), canvas.height - (canvas.height * 0.10), paddleWidth, paddleHeight, "white");
+    createBox((canvas.width/2) - (paddleWidth / 2),             // X coordinate
+              canvas.height - (canvas.height * paddleY_offset), // Y coordinate
+              paddleWidth,                                      // width
+              paddleHeight,                                     // height
+              "white");                                         // colour
     //player 2 paddle, top
-    createBox(canvas.width/2 - (paddleWidth / 2), (canvas.height * 0.10), paddleWidth, paddleHeight, "white");
+    createBox((canvas.width/2) - (paddleWidth / 2),             // X coordinate
+              (canvas.height * paddleY_offset),                 // Y coordinate
+              paddleWidth,                                      // width
+              paddleHeight,                                     // height
+              "white");                                         // colour
     
     //create the center line
     drawNet();
     
+    //create the score board
+    
     //create the ball
     createBox(ballXcoord, ballYcoord, ballWidth, ballHeight, "red");
     
+}
+
+//used when the game has ended.
+function reset()
+{
     
-    //create the score board
 }
 
 //move the ball and the paddle
 function moveElements()
 {
     //move the ball
-    ballYcoord += 5;
+//    ballYcoord -= ballYSpeed;       // minus so it can move up
+    ballXcoord += ballXSpeed;
+    
+    //code to bounce the ball of the side walls (left and right)
+    if(ballXcoord < 0)
+    {
+        //flip the X value
+        ballXSpeed = -ballXcoord;
+    }
+    if(ballXcoord > canvas.width)
+    {
+        ballXSpeed = -ballXSpeed;
+    }
     
 }
 
@@ -87,16 +114,17 @@ function drawNet()
 {
     for(var i = 0; i < canvas.width; i += 40)
     {
-//        createBox(canvas.height / 2, i, 2, 20, "white");
         createBox(i, canvas.height/2-1, 20, 2, "white");
     }
 }
 
+//moving player 1 paddle
 function moveP1()
 {
     
 }
 
+//moving player 2 paddle
 function moveP2()
 {
     

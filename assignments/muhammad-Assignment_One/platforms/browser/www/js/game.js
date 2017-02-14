acead80407679a5974118483c3d9428a48b9fbfc
Clumsy-Coder <ubari94@gmail.com>
2017-02-13 20:56:03 -0700

    Make progress with canvas JavaScript and more
    - JavaScript
      * Set canvas width and height when loading the page.
        > So it can work on multiple devices.
        > NOTE: It's NOT responsive, in the sense resizing the browser will
          NOT scale the canvas elements accordingly. It only works when loading the page.
      * Create two paddle.
        > One on the bottom (player 1, the user) and the top (player 2, the computer)
      * Create the ball
        > In a square shape.
        > Needs some tuning on placing the ball in the center of the screen
      * Draw a center line.
        > Nice thing to have.
        > Need to be sure it's in the center.
      * Set the ball movement to 60 frames per second
        > Not currently being used at the moment.
    
    - Minor changes
      * Add some CSS
        > for now trying to make sure there's no padding or margin on the canvas
          > To prevent scrolling
          > Not successful at the moment
      * Add 'id' attribute for the game div
        > used for fine tuning the padding or margin
        > need to fine tune later

--- assignments/muhammad-Assignment_One/platforms/browser/www/js/game.js
+++ assignments/muhammad-Assignment_One/platforms/browser/www/js/game.js
@@ -5,12 +5,14 @@ var cContext;
 //paddle
 var p1Paddle;
 var p2Paddle;
-const paddleWidth = 2;
-const paddleHeight = 10;
+const paddleWidth = 100;
+const paddleHeight = 15;
 
 //ball
 var ballXcoord;
 var ballYcoord;
+const ballWidth = 20;
+const ballHeight = 20;
 
 //score
 var p1Score;
@@ -22,19 +24,27 @@ var p1Name;
 //frames per second
 const FPS = 60;
 
+//functions
+
 
 window.onload = function()
 {
+    $("#gameCanvas").attr("width", window.innerWidth);
+    $("#gameCanvas").attr("height", window.innerHeight);
+    
+    console.log(document.getElementById("gameCanvas"));
     canvas = document.getElementById("gameCanvas");
     cContext = canvas.getContext("2d");
     //set the ball x and y coordinate
     ballXcoord = canvas.width / 2;
     ballYcoord = canvas.height / 2;
-    setInterval(function() {
-                            moveElements();
-                            setup();
-                           }
-                , 1000 / FPS);
+    setInterval(init(), 1000 / FPS);
+}
+
+function init()
+{
+    moveElements();
+    setup();
 }
 
 //create the objects of the game
@@ -44,11 +54,17 @@ function setup()
     createBox(0, 0, canvas.width, canvas.height, "black");
     
     //create the paddle
-    
-    //create the ball
-    createBox(ballXcoord, ballYcoord, 10, 10, "red");
+    //player 1 paddle, bottom
+    createBox(canvas.width/2 - (paddleWidth / 2), canvas.height - (canvas.height * 0.10), paddleWidth, paddleHeight, "white");
+    //player 2 paddle, top
+    createBox(canvas.width/2 - (paddleWidth / 2), (canvas.height * 0.10), paddleWidth, paddleHeight, "white");
     
     //create the center line
+    drawNet();
+    
+    //create the ball
+    createBox(ballXcoord, ballYcoord, ballWidth, ballHeight, "red");
+    
     
     //create the score board
 }
@@ -56,13 +72,24 @@ function setup()
 //move the ball and the paddle
 function moveElements()
 {
+    //move the ball
+    ballYcoord += 5;
     
 }
 
 function createBox(xCoord, yCoord, width, height, colour)
 {
-    cContext.fullStyle(colour);
+    cContext.fillStyle = colour;
     cContext.fillRect(xCoord, yCoord, width, height);
+}
+
+function drawNet()
+{
+    for(var i = 0; i < canvas.width; i += 40)
+    {
+//        createBox(canvas.height / 2, i, 2, 20, "white");
+        createBox(i, canvas.height/2-1, 20, 2, "white");
+    }
 }
 
 function moveP1()

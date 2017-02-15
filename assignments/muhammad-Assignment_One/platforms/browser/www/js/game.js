f8980d6550ce8b19b3683813b1f835db7b26a89d
Clumsy-Coder <ubari94@gmail.com>
2017-02-14 22:27:11 -0700

    Implement code to bounce ball of the paddle and more
    - ball bouncing of the paddle
      * Getting the ball to bounce of the paddle works
        > needs some tweaking to get it to work better
      * this works for player 1 (user) and player 2 (computer)
    - Reset the ball when a player scores
      * The ball goes towards the player who just scored
      * NOTE: a bug was found when player 1 scores, the ball is going towards player 2.
        It should be the opposite.
      * The ball gets placed at the center of the game when starting the next round.
    - Refactor code
      * move some of the code into different functions. To make it more organized and easy to read.
        The functions created:
        > reset()
        > moveBall()
        > checkPlayer1()
        > checkPlayer2()
        > sideWallBounce()
        > moveP1()
        > moveP2()

--- assignments/muhammad-Assignment_One/platforms/browser/www/js/game.js
+++ assignments/muhammad-Assignment_One/platforms/browser/www/js/game.js
@@ -18,8 +18,9 @@ var ballXSpeed = 5;
 var ballYSpeed = -5;    //negative to move the ball up.
 
 //score
-var p1Score;
-var p2Score;
+var p1Score = 0;
+var p2Score = 0;
+var scoreThreshold = 10;
 
 //player name
 var p1Name;
@@ -41,17 +42,7 @@ window.onload = function()
     ballYcoord = canvas.height / 2;
     p1PaddleX = (canvas.width / 2) - PADDLE_WIDTH / 2;
     p2PaddleX = (canvas.width / 2) - PADDLE_WIDTH / 2;
-    setInterval(init, 1000/FPS);
-    
-    //get the mouse position to move player 1 paddle
-    //supports desktop and mobile device
-    $(document).on('vmousemove', function(event){
-        p1PaddleX = event.pageX - (PADDLE_WIDTH / 2);
-    });
-
-    $(document).on('taphold', function(e){
-        p1PaddleX = e.pageX - (PADDLE_WIDTH / 2);
-    });
+    setInterval(init, 1000 / FPS);
 
 }
 
@@ -86,52 +77,142 @@ function setup()
     
     //create the score board
     
+    
     //create the ball
-    createBox(ballXcoord, ballYcoord, ballWidth, ballHeight, "red");
+    createBox(ballXcoord - (ballWidth / 2),                     // X coordinate
+              ballYcoord - (ballHeight / 2),                    // Y coordinate
+              ballWidth,                                        // width
+              ballHeight,                                       // height
+              "red");                                           // colour
     
 }
 
 //used when the game has ended.
 function reset()
 {
+    //check who won
+    //if player 2 won
+    if(ballYcoord < (canvas.height * paddleY_offset) - PADDLE_HEIGHT)
+    {
+        //switch the ball to player 1.
+        ballYSpeed = -ballYSpeed;
+
+    }
+    //if player 1 won
+    else if(ballYcoord > (canvas.height - (canvas.height * paddleY_offset)) + PADDLE_HEIGHT)
+    {
+        //switch the ball to player 2.
+        ballYSpeed = -ballYSpeed;
+    }
     
+    //reset the position of the ball
+    ballXcoord = canvas.width / 2;
+    ballYcoord = canvas.height / 2;
+    ballXSpeed = 5;
 }
 
 //move the ball and the paddle
 function moveElements()
 {
+    moveBall();
+    moveP1();
+    moveP2();
+}
+
+function moveBall()
+{
     //move the ball
     ballXcoord += ballXSpeed;
     ballYcoord += ballYSpeed;
     
+    //check if the ball is touching the side walls
+    sideWallBounce();
+    
+    //check if the ball is close to player 2.
+    //  check if it touched the paddle or player 1 scored.
+    checkPlayer2();
+    
+    //check if the ball is close to player 1.
+    //  check if it touched the paddle or player 2 scored.
+    checkPlayer1();
+    
+}
+
+function checkPlayer1()
+{
+    //if the ball is close to player 1
+    if(ballYcoord > (canvas.height - (canvas.height * paddleY_offset)) + PADDLE_HEIGHT)
+    {
+        //check if player's paddle was there.
+        if(ballXcoord > p1PaddleX && 
+           ballXcoord < p1PaddleX + PADDLE_WIDTH)
+        {
+            ballYSpeed = -ballYSpeed;
+            var deltaX = ballXcoord - (p1PaddleX + (PADDLE_WIDTH / 2));
+            ballXSpeed = deltaX * 0.35;
+        }
+        
+//        //player 2 scored
+        else
+        {
+            p2Score++;
+            reset();
+        }
+    }
+    
+    //player 2 scored
+//    if(ballYcoord > canvas.height)
+//    {
+//        p2Score++;
+//        reset();
+//    }
+}
+
+function checkPlayer2()
+{
+    //if the ball is close to player 2
+    if(ballYcoord < (canvas.height * paddleY_offset) + PADDLE_HEIGHT)
+    {
+        //check if player's 2 paddle was there.
+        if(ballXcoord > p2PaddleX && 
+           ballXcoord < p2PaddleX + PADDLE_WIDTH)
+        {
+            ballYSpeed = -ballYSpeed;
+            var deltaX = ballXcoord - (p2PaddleX + (PADDLE_WIDTH / 2));
+            ballXSpeed = deltaX * 0.35;
+        }
+        
+        //player 1 scored
+        else
+        {
+            p1Score++;
+            reset();
+        }
+    }
+    
+    //player 1 scored
+//    if(ballYcoord < 0)
+//    {
+//        p1Score++;
+//        reset();
+//    }
+}
+
+//bounce the ball of the side
+function sideWallBounce()
+{
     //code to bounce the ball of the side walls (left and right)
     if(ballXcoord < 0)
     {
-        //flip the X value
         ballXSpeed = -ballXcoord;
     }
     if(ballXcoord > canvas.width)
     {
         ballXSpeed = -ballXSpeed;
     }
-    //testing if the ball moves up and down when touching the top or bottom wall
-    if(ballYcoord < 0)
-    {
-        //flip the X value
-        ballYSpeed = -ballYcoord;
-    }
-    if(ballYcoord > canvas.height)
-    {
-        ballYSpeed = -ballYSpeed;
-    }
 }
 
-function createBox(xCoord, yCoord, width, height, colour)
-{
-    cContext.fillStyle = colour;
-    cContext.fillRect(xCoord, yCoord, width, height);
-}
-
+//draw a net in the center of the page
 function drawNet()
 {
     for(var i = 0; i < canvas.width; i += 40)
@@ -143,11 +224,35 @@ function drawNet()
 //moving player 1 paddle
 function moveP1()
 {
-    
+    //get the mouse position to move player 1 paddle
+    //supports desktop and mobile device
+    $(document).on('vmousemove', function(event){
+        p1PaddleX = event.pageX - (PADDLE_WIDTH / 2);
+    });
+
+    $(document).on('taphold', function(e){
+        p1PaddleX = e.pageX - (PADDLE_WIDTH / 2);
+    });
 }
 
 //moving player 2 paddle
 function moveP2()
 {
-    
+    var p2PaddleX_center = p2PaddleX + (PADDLE_WIDTH / 2);
+    // +/- 35 so the computer paddle won't jitter
+    if(p2PaddleX_center < ballXcoord - 35)
+    {
+        p2PaddleX += 6;
+    }
+    else if(p2PaddleX_center > ballXcoord + 35)
+    {
+        p2PaddleX -= 6;
+    }
+}
+
+//use canvas context to create and place the rectangle/square in the canvas
+function createBox(xCoord, yCoord, width, height, colour)
+{
+    cContext.fillStyle = colour;
+    cContext.fillRect(xCoord, yCoord, width, height);
 }
